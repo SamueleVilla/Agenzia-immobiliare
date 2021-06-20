2f79f2f50d33f1c6389377edfc154ab78d946ea0
Samuele Villa <samuele.villa.ufficale@gmail.com>
2021-06-20 12:17:13 +0200

    done

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # main module python
-# __main__ scipt
+
 
 from Agenzia.agency import Agenzia
 from Agenzia.models import *
@@ -8,7 +8,7 @@ myAgenzy = Agenzia("Villa cars & motors", "Milano")
 
 casa = Immobile("1234AADD", "Via Riviera 18", 28053, "Roma", 500)
 mybox = Box("AADDAAEE", "Via Riviera 18", 2798, "Roma", 200, 10)
-myApart = Appartamento("AAEE12445", "Via di culo 69",
+myApart = Appartamento("AAEE12445", "Via caduti per la libertà 69",
                        9876, "Roma", 400, 6, 2)
 myvilla = Villa("KKJAAD22", "Via dei ricconi 32",
                 2976, "Milano", 1000, 10, 7, 500)
@@ -20,3 +20,5 @@ myAgenzy.add_immobile(myvilla)
 
 for immobile in myAgenzy.search_immobili("Via Riviera 18"):
     print(immobile.scheda_immobile(), end="\n\n")
+
+# end and it works :)
